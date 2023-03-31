45f2842b0d2f13b0b53134d560126fab72cecff2
Jorg3333 <129445060+Jorg3333@users.noreply.github.com>
2023-03-31 19:46:27 -0400

    Clase 2: Dinosaurio

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,7 +1,7 @@
 import pygame
 
 from dino_runner.utils.constants import BG, CLOUD, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
-
+from dino_runner.components.dinosaur import Dinosaur
 
 class Game:
     def __init__(self):
@@ -14,9 +14,10 @@ class Game:
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
-        self.x_pos_cloud = 100
-        self.y_pos_cloud = 150
-       
+        self.x_pos_cloud = 0
+        self.y_pos_cloud = 380
+        self.player = Dinosaur()
+        #self.user_input = 0
 
     def run(self):
         # Game loop: events - update - draw
@@ -33,13 +34,15 @@ class Game:
                 self.playing = False
 
     def update(self):
-        pass
+        user_input = pygame.key.get_pressed()
+        self.player.update(user_input)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
         self.draw_cloud()
+        self.player.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
